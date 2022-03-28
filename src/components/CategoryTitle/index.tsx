49e06dc8fa6528f0bfdae97ca345ed10c430caa7
falexandre <fabiolalexandre@gmail.com>
2022-03-28 10:39:21 -0300

    List component Posts and init footer

---
+++ src/components/CategoryTitle/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container, More, Title } from './styles';
+import { Button, Icon, Text } from 'native-base';
+import { MaterialIcons } from '@expo/vector-icons';
+
+interface Props {
+    title: string;
+}
+
+export function CategoryTitle(props: Props) {
+    const { title } = props;
+    return (
+        <Container>
+            <Title>{title}</Title>
+            <More>
+                <Text>VER MAIS</Text>
+                <Icon as={MaterialIcons} name="arrow-right" size="md" />
+            </More>
+        </Container>
+    );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ export { Main } from './Main';
 export { Filter } from './Filter';
 export { CategoryList } from './CategoryList';
 export { CardPost } from './CardPost';
+export { CategoryTitle } from './CategoryTitle';
 export { Footer } from './Footer';
